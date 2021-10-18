2171743f69dffe2b3880390279114dfa1b4b9dc7
Jordan Woerndle <jordan52@gmail.com>
2021-10-18 11:49:13 -0500

    added a python server..  it kind of works

---
+++ pythonserver/main.py
@@ -0,0 +1,33 @@
+import grpc
+import sys
+import os
+from concurrent import futures
+# I don't like this, but it is the simplest way to grab the proto files.
+sys.path.append(os.path.dirname(os.path.dirname(__file__)) + '../protobuf')
+
+from proto import status_pb2
+from proto import statusservice_pb2_grpc
+
+
+
+
+class StatusSerivceServicer(statusservice_pb2_grpc.StatusServiceServicer):
+    """Provides methods that implement functionality of route guide server."""
+
+    def __init__(self):
+        print("initializing status service")
+
+    def GetStatus(self, request, context):
+        print("Got a status!")
+
+def run():
+
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    statusservice_pb2_grpc.add_StatusServiceServicer_to_server(
+        StatusSerivceServicer(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
+
+if __name__ == '__main__':
+    run()
